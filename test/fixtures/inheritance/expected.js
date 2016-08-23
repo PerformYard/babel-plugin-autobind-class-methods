3caca8c385c60097b6e6e9de69c17e33a919483f
UgnisSoftware <karolis.masiulis@ugnis.com>
2016-08-23 19:44:16 +0300

    Fix spaces in tests

--- test/fixtures/inheritance/expected.js
+++ test/fixtures/inheritance/expected.js
@@ -15,56 +15,56 @@ function _makeJsClassGreatAgain(_this, funcName, params) {
 var Simple = function () {
   function Simple() {
     var _this2 = this;
-    
+
     _classCallCheck(this, Simple);
-    
+
     this.greet = function () {
       for (var _len = arguments.length, rest = Array(_len), _key = 0; _key < _len; _key++) {
         rest[_key] = arguments[_key];
       }
-      
+
       return _makeJsClassGreatAgain(_this2, 'greet', rest);
     };
   }
-  
+
   _createClass(Simple, [{
     key: 'greet',
     value: function greet() {
       console.log(this.greeting);
     }
   }]);
-  
+
   return Simple;
 }();
 
 var Example = function (_Simple) {
   _inherits(Example, _Simple);
-  
+
   function Example() {
     _classCallCheck(this, Example);
-    
+
     var _this3 = _possibleConstructorReturn(this, Object.getPrototypeOf(Example).call(this));
-    
+
     _this3.greeting = 'Hello';
-    
+
     _this3.render = function () {
       for (var _len2 = arguments.length, rest = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
         rest[_key2] = arguments[_key2];
       }
-      
+
       return _makeJsClassGreatAgain(_this3, 'render', rest);
     };
-    
+
     return _this3;
   }
-  
+
   _createClass(Example, [{
     key: 'render',
     value: function render() {
       setTimeout(this.greet, 0);
     }
   }]);
-  
+
   return Example;
 }(Simple);
 
